15a53124ae9ab61eaebfe929f0ad59c58165f300
dnakano <dnakano@student.42tokyo.jp>
2021-12-17 10:05:13 +0900

    add tracking server

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 
 	"github.com/dai65527/vast-server-sample/adserver"
 	"github.com/dai65527/vast-server-sample/logger"
+	"github.com/dai65527/vast-server-sample/trackingserver"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("public")))
-	mux.Handle("/ad/vast", &adserver.AdServer{})
+	mux.Handle("/", http.FileServer(http.Dir("public")))      // 動画配信サーバ
+	mux.Handle("/ad/vast", &adserver.AdServer{})              // 広告配信サーバ
+	mux.Handle("/tracking", &trackingserver.TrackingServer{}) // トラッキングサーバ
 
 	err := http.ListenAndServe(":8080", logger.HttpLogger(mux))
 	log.Printf("server error: %v", err)
---
+++ trackingserver/trackingserver.go
@@ -0,0 +1,21 @@
+package trackingserver
+
+import (
+	"log"
+	"net/http"
+)
+
+type TrackingServer struct{}
+
+var _ http.Handler = (*TrackingServer)(nil)
+
+// ServeHTTP 広告視聴ログを記録
+func (s *TrackingServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// クエリパラメタを取得
+	adID := r.URL.Query().Get("userID")
+	userID := r.URL.Query().Get("userID")
+	event := r.URL.Query().Get("event")
+
+	// 広告視聴ログを記録
+	log.Println("[ad watch log]", "ad", adID, "user", userID, "event", event)
+}
